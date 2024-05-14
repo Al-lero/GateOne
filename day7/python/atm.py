c1e09ddaa20f368542fafc7a5c929fdcac5da384
Al-lero <aleroutieyione@gmail.com>
2024-05-14 04:34:13 +0100

    day7

--- day7/python/atm.py
+++ day7/python/atm.py
@@ -1,5 +1,5 @@
 balance = 0
-withdrwa = 0
+withdraw = 0
 deposit = 0
 transfer = 0
 
@@ -7,7 +7,8 @@ transaction_list = []
 
 print("Welcome to Lero's Banking Platform")
 
-menuDisplay = print('''
+
+menu_display = print('''
 
 	1. create an Account
 	2. close Account
@@ -20,4 +21,58 @@ menuDisplay = print('''
 	9. View Transcations
 	''')
 
-inputt = int(input("Enter your preffered number: "))
+
+user_input = int(input("Enter your preffered number: "))
+
+balance = deposit - withdraw
+	
+if user_input == 1:
+	print("To create new Account")
+	first_name = str(input("Enter firstName: "))
+	last_name = str(input("Enter lastName: "))
+
+	pin = int(input("Enter 4 numbers for pin: "))
+	if pin != 4:
+		print("Invalid pin")
+	else:
+		print("Account opened successfully")
+		transaction_list.append(f"Account opened by  {first_name} {last_name}")
+
+elif user_input == 2:
+	print("Account sucessfully closed")
+	transaction_list.append('Account closed')
+
+elif user_input == 3:
+	deposit = float(input("Deposit money: " ))
+	print(f"Balance : " , deposit)
+	transaction_list.append(f'Deposited: {deposit}')
+
+elif user_input == 4:
+	withdraw = float(input("Enter amount to withdraw: "))
+	print(f"Balance: " , balance)
+	transaction_list.append(f'Withdrawn: {withdraw}')
+
+elif user_input == 5:
+	print(f"Check Acount balance: " , balance)
+
+elif user_input == 6:
+	transfer = float(input("Enter amount to transfer: "))
+	print("Your balance is : ", balance)
+	transcation_list.append(f'Transferred: {transfer}')
+
+elif user_input == 7:
+	change_pin =int(input("Enter new pin: "))
+	print("Pin change sucessfully")
+
+elif user_input == 8:
+	print("Exiting....")
+	exit(0)
+
+elif user_input == 9:
+	print("Transcations: ")
+	for transcation in transction_list:
+		print(transcation)
+
+else:
+	print("Invalis option")
+	 
