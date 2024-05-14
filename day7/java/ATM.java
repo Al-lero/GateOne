db5cc8720d56417f5823685b9d46e317282d10a6
Al-lero <aleroutieyione@gmail.com>
2024-05-14 02:53:00 +0100

    day7

--- day7/java/ATM.java
+++ day7/java/ATM.java
@@ -2,127 +2,119 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-	public class ATM {
 
+	public class A {
 		public static void main(String[] args){
-		
+        	double balance = 0;
+        	double withdraw = 0;
+       		double deposit = 0;
+       		double transfer = 0;
 
-		double balance = 0;
-		double withdraw = 0;
-		double deposit = 0;
-		double transfer = 0;
+        	ArrayList<String> listOfTransactions = new ArrayList<>();
 
-		ArrayList<Customer> accounts = new ArrayList<>();
+       		Scanner alero = new Scanner(System.in);
 
-		
+        	while(true) {
+            		System.out.println("Welcome to Lero's Banking Platform");
 
-		System.out.println("Welcome to Lero's Banking Platform");
-	
-		
-			String mainMenu = """
-			press
-				1-> Create an Account
-				2-> Close Account
-				3-> Deposit Money
-				4-> Withdraw Money
-				5-> Check Account Balance
-				6-> Transfer from one account to another
-				7-> Change pin
-				
-				
-				""";
-			
+            		String mainMenu = """
+           		press
+			    1-> Create an Account
+                	    2-> Close Account
+               	 	    3-> Deposit Money
+                	    4-> Withdraw Money
+                	    5-> Check Account Balance
+                	    6-> Transfer from one account to another
+                	    7-> Change pin
+               	 	    8-> Exit
+                	    9-> View Transactions
+                	    """;
 
-				System.out.println(mainMenu);
+            		System.out.println(mainMenu);
 
-				
+            		System.out.println("Enter your preferred number: ");
+            		int userInput = alero.nextInt();
 
-				Scanner alero = new Scanner(System.in);
+            		balance = deposit - withdraw;
 
+           	 	switch(userInput) {
+                		case 1 -> {
+                    			System.out.println("To create new account");
+                    			alero.nextLine();
+                    			System.out.println("Enter firstName: ");
+                    			String firstName = alero.nextLine();
 
-				System.out.println("Enter your preffered number: ");
-				int userInput = alero.nextInt();
-			
-				balance = deposit - withdraw;
+                    			System.out.println("Enter lastName: ");
+                    			String lastName = alero.nextLine();
 
-				
-			
-			switch(userInput) {
-				case 1 -> {
-					System.out.println("To create new account");
-					 alero.nextLine();
-					System.out.println("Enter firstName: ");
-					String firstName = alero.nextLine();
-
-					System.out.println("Enter lastName: ");
-					String lastName = alero.nextLine();
-
-					System.out.println("Enter 4 numbers for pin: ");
-					int number = alero.nextInt();
-
-					System.out.println("Account opened successfully");
-				
+                    			System.out.println("Enter 4 numbers for pin: ");
+                    			int number = alero.nextInt();
+		    			if (number > 4 || number < 4){
+		    			System.out.print("invalid pin ");
 					}
-				
-	
-				case 2 -> {
-					System.out.println("Account successfully closed");
-				}
-				
 
-				case 3 -> {
-					System.out.println("Deposit money: ");
-				 	deposit = alero.nextDouble();
-				
-					System.out.println("Balance : " + deposit);
-				}
-				
+                   			System.out.println("Account opened successfully");
+                    			listOfTransactions.add("Account opened by " + firstName + " " + lastName);
+               			 }
 
-				case 4 -> {
-					System.out.println("Withdraw money: ");
-		 			withdraw = alero.nextDouble();
+                		case 2 -> {
+                    			System.out.println("Account successfully closed");
+                    			listOfTransactions.add("Account closed");
+                		}
 
-					System.out.println("Balance: " + balance);
- 
-				}
+                		case 3 -> {
+                    			System.out.println("Deposit money: ");
+                    			deposit = alero.nextDouble();
 
-				case 5 -> {
-					System.out.println("Check Account balance: " + balance);
-				
-				}
+                    			System.out.println("Balance : " + deposit);
+                    			listOfTransactions.add("Deposited: " + deposit);
+                		}
 
-				case 6 -> {
-					System.out.println("Amount to transfer: ");
-					transfer = alero.nextInt();
-		
-					System.out.println("Your balance is: " + balance);
-				
-				}
+                		case 4 -> {
+                    			System.out.println("Withdraw money: ");
+                    			withdraw = alero.nextDouble();
 
-				case 7 -> {
-					System.out.println("Change pin: ");
-					int pin = alero.nextInt();
+                    			System.out.println("Balance: " + balance);
+                    			listOfTransactions.add("Withdrawn: " + withdraw);
+                		}
 
-					System.out.println("Pin changed successfully");
-				
-				}
+                		case 5 -> {
+                    			System.out.println("Check Account balance: " + balance);
+                		}
 
-				case 8 -> {
-					System.out.print("Enter 0 to go back to mainMenu: ");
-					int numberEntered = alero.nextInt();
-					if (numberEntered == 0){
-					System.out.println(mainMenu);
+                		case 6 -> {
+                    			System.out.println("Amount to transfer: ");
+                    			transfer = alero.nextInt();
 
-					System.out.println("Enter your preffered number: ");
-					userInput = alero.nextInt();
-			}
-			
-				
+                    			System.out.println("Your balance is: " + balance);
+                    			listOfTransactions.add("Transferred: " + transfer);
+                		}
+
+                		case 7 -> {
+                    			System.out.println("Change pin: ");
+                    			int pin = alero.nextInt();
+
+                    			System.out.println("Pin changed successfully");
+                    			listOfTransactions.add("Pin changed");
+                		}
+
+                		case 8 -> {
+                    			System.out.println("Exiting...");
+                    			System.exit(0);
+               			 }
+
+               			 case 9 -> {
+                    			System.out.println("Transactions: ");
+                    			for(String transaction : listOfTransactions) {
+                        			System.out.println(transaction);
+                    		}
+                	}
+            	}
+        }
+    }
 }
-}
+				
 
-}
-}
 
 
 				
