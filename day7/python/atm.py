73be04bad0a1b91d410687bfebb8c79ef329ad3b
Al-lero <aleroutieyione@gmail.com>
2024-05-14 03:07:41 +0100

    day7

---
+++ day7/python/atm.py
@@ -0,0 +1,23 @@
+balance = 0
+withdrwa = 0
+deposit = 0
+transfer = 0
+
+transaction_list = []
+
+print("Welcome to Lero's Banking Platform")
+
+menuDisplay = print('''
+
+	1. create an Account
+	2. close Account
+	3. Deposit Money
+	4. Withdraw Money
+	5. Check Account Balance
+	6. Transger from one account to another
+	7. Change pin
+	8. Exit
+	9. View Transcations
+	''')
+
+inputt = int(input("Enter your preffered number: "))
